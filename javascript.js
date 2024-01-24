893735ac4f21063f4e0bcad7f4b915222810f964
mustafa Jabr <jabrmustafa@gmail.com>
2024-01-24 18:02:12 -0500

    part 2

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
 console.log('hello world');
+const form = document.querySelector('#form')
+const submitbutton = document.querySelector('#submit')
+
+form.addEventListener('submit',(e) => {submitbutton.disabled - true e.preventDefault()window.location.href = window.location.origin+ '/succes.html'})
